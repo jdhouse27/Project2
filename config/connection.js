a4d225a33262a84ea9153721d72f5df047b4b542
JD <jdhouse27@gmail.com>
2019-06-06 19:59:08 -0500

    added files and made some initial changes

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ if (process.env.JAWSDB_URL) {
         port: 3306,
         user: "root",
         password: event.password,
-        database: "event_db"
+        database: "events_db"
     });
 };
 // Make connection.
--- models/category.js
+++ models/category.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-    var Category = sequelize.define("Category", {
+    var Event = sequelize.define("Event", {
         text: DataTypes.STRING,
         description: DataTypes.TEXT
     });
-    return Category;
+    return Event;
 };
