ade901cf30330bc12f07a52213707d4ad359b18a
Matt Vanley <matt@vibrantsitedesign.com>
2019-06-06 20:28:20 -0500

    login

---
+++ public/js/firebase.js
@@ -0,0 +1,17 @@
+var app_fireBase = {};
+(function(){
+   var config = {
+       apiKey: "AIzaSyDVhHGHb6hXl3LvveyiAWp3DLzNIp7yp6I",
+       authDomain: "scenic-pro2.firebaseapp.com",
+       databaseURL: "https://scenic-pro2.firebaseio.com",
+       projectId: "scenic-pro2",
+       storageBucket: "",
+       messagingSenderId: "755781268373",
+       appId: "1:755781268373:web:ca9d87ccca610859"
+     };
+
+     firebase.initializeApp(config);
+
+     app_fireBase = firebase;
+})()
+
